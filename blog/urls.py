12925a5b073534f17e21de59acde82486e8550d5
shahrokh <ashayeri.eng@gmail.com>
2025-08-02 12:21:00 +0330

    add Homepage url

--- blog/urls.py
+++ blog/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from post.views import PostListView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('posts/', include('post.urls', namespace='post')),
     path('acc/', include('acc.urls', namespace='acc')),
     path('api/', include('api.urls', namespace='api')),
+    path('', PostListView.as_view(), name="homepage")
 ]
