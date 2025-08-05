b038c23d858ccc5724613376c8153d924340fc3a
shahrokh <ashayeri.eng@gmail.com>
2025-08-05 15:06:34 +0330

    add most commented post tag and edit the related template to show that

--- post/templatetags/blog_tags.py
+++ post/templatetags/blog_tags.py
@@ -1,13 +1,23 @@
 from post.models import Post
 from django import template
+from django.db.models import Count
 
 register = template.Library()
+
 
 @register.simple_tag
 def total_count():
     return Post.published.count()
 
-@register.inclusion_tag('post/latest_posts.html')
+
+@register.inclusion_tag("post/latest_posts.html")
 def show_latest_posts(count=5):
-    latest_posts =  Post.published.order_by("-published_time")[:count]
-    return {'latest_posts': latest_posts}
+    latest_posts = Post.published.order_by("-published_time")[:count]
+    return {"latest_posts": latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count("comments")).order_by(
+        "-total_comments"
+    )[:count]
